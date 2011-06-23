47df66f10cfa38728ba45e09d2885fc8cc1fa24b
Eric Andrews <harbu88@hotmail.com>
2011-06-23 19:43:36 +0300

    DRYed up state spec

--- test/state_spec.rb
+++ test/state_spec.rb
@@ -1,33 +1,37 @@
 # encoding: utf-8
 require 'state'
 
-describe State, "on instantiation" do
-  it "should have a transition to itself for all alphabet symbols" do
-    state = State.new ['a','b']
-    state.transition('a').should == state
-    state.transition('b').should == state
+describe State do
+  
+  before(:each) do
+    @state = State.new ['a', 'b']
+  end
+  
+  describe "on instantiation" do
+    it "should have a transition to itself for all alphabet symbols" do
+      @state.transition('a').should == @state
+      @state.transition('b').should == @state
+    end
+  end
+
+  describe "when assigning transitions" do
+    it "should raise an exception if given a symbol that doesn't belong to its alphabet" do
+      lambda { @state.change_transition('c', @state) }.should
+        raise_error RuntimeError, "Symbol 'c' not in state's alphabet"
+    end
+    
+    it "should raise an exception if given a state that has a different alphabet" do
+      state_two = State.new ['a', 'b', 'c']
+      lambda { @state.change_transition('a', state_two) }.should
+        raise_error RuntimeError, "States have uncompatible alphabets"
+    end
+    
+    it "should successfully change transitions on valid arguments" do
+      state_two = State.new ['b', 'a']
+      @state.change_transition('b', state_two)
+      @state.transition('b').should == state_two
+    end
   end
 end
 
-describe State, "when assigning transitions" do
-  it "should raise an exception if given a symbol that doesn't belong to its alphabet" do
-    state = State.new ['a', 'b']
-    lambda { state.change_transition('c', state) }.should
-      raise_error RuntimeError, "Symbol 'c' not in state's alphabet"
-  end
-  
-  it "should raise an exception if given a state that has a different alphabet" do
-    state = State.new ['a', 'b']
-    state_two = State.new ['a', 'b', 'c']
-    lambda { state.change_transition('a', state_two) }.should
-      raise_error RuntimeError, "States have uncompatible alphabets"
-  end
-  
-  it "should successfully change transitions on valid arguments" do
-    state = State.new ['a', 'b']
-    state_two = State.new ['b', 'a']
-    state.change_transition('b', state_two)
-    state.transition('b').should == state_two
-  end
-    
-end
+
