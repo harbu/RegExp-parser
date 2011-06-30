69272171cf683848976f58a60058293de196387c
Eric Andrews <harbu88@hotmail.com>
2011-07-01 02:59:15 +0300

    some refactoring of runner

--- lib/automaton_runner.rb
+++ lib/automaton_runner.rb
@@ -16,28 +16,28 @@ module Automaton
     end
     
   private
-    def string_compatible?(string)
-      string.chars.to_a.uniq.all? {|symbol| @automaton.compatible_symbol?(symbol) }
-    end
-    
     def traverse(string, state)
       if string.empty?
         if @automaton.accept_state?(state)
           true
         else
-          state.transitions[:epsilon].find do |next_state|
-            traverse(string, next_state)
-          end
+          traverse_epsilon(string, state)
         end
       else
-        symbol = string[0]
-        res1 = state.transitions[:epsilon].find do |next_state|
-          traverse(string, next_state)
-        end
-        res2 = state.transitions[symbol].find do |next_state|
-          traverse(string[1..-1], next_state)
-        end
-        res1 || res2
+        traverse_symbol(string, state) || traverse_epsilon(string, state)
+      end
+    end
+    
+    def traverse_symbol(string, state)
+      symbol = string[0]
+      res = state.transitions[symbol].find do |next_state|
+        traverse(string[1..-1], next_state)
+      end
+    end
+    
+    def traverse_epsilon(string, state)
+      state.transitions[:epsilon].find do |next_state|
+        traverse(string, next_state)
       end
     end
   end
